11a8079c92df38dc5b9e5b74a239d2722c9a4e88
Trevor Assaf <astrev@umich.edu>
2016-11-22 13:55:40 -0800

    Added PinManager::BindFunction() method

--- include/PinManager.h
+++ include/PinManager.h
@@ -16,14 +16,32 @@ class PinManager {
 		PinManager();
 		~PinManager();
 
-		PinManager& SetFunction(uint8_t pin_index, PinType pin_type);
+    // Configures pin for designated function.
+		PinManager& BindFunction(uint8_t pin_index, PinType pin_type);
+
+  private:
+    inline uint32_t MakeClearFunctionValue(size_t shift_value) const;
+    inline uint32_t MakeSetFunctionValue(size_t shift_value, PinType pin_type) const;
+    inline size_t MakeShiftValue(uint8_t pin_index) const;
+    inline size_t MakeFunctionRegisterIndex(uint8_t pin_index) const;
+
 	private:
-    static constexpr uint64_t GPIO_MEMORY_OFFSET = 0xF2200000;
+    // BCM2835 maps gpio peripherals into this address in physical memory. We will access this region
+    // of physical memory by applying mmap() to /dev/mem
+    static constexpr uint64_t GPIO_PHYSICAL_MEMORY_OFFSET = 0x2200000;
+
+    // We will map an entire page into memory.
     static constexpr size_t PAGE_SIZE = 4096;
-		static constexpr size_t PIN_FUNCTION_REG_COUNT = 5;
+
+    // There are 5 memory locations that modulate the functions of the gpio pins. We protect address
+    // with a mutex.
+		static constexpr size_t PIN_FUNCTION_REGISTER_COUNT = 5;
+    static constexpr size_t PINS_PER_FUNCTION_REGISTER = 10;
 		std::vector<std::mutex> pin_function_locks_;
 
-    volatile uint64_t*  gpio_base_;
+    static constexpr size_t BITS_PER_FUNCTION = 3;
+
+    volatile uint32_t*  gpio_base_;
 };
 
 } // namespace gpio
--- src/PinManager.cpp
+++ src/PinManager.cpp
@@ -9,6 +9,7 @@
 #include <cassert>
 #include <cstring>
 #include <stdexcept>
+#include <type_traits>
 
 #include "SysUtils.h"
 #include "ScopeGuard.h"
@@ -16,7 +17,7 @@
 namespace gpio {
 
 PinManager::PinManager()
-	: pin_function_locks_(PIN_FUNCTION_REG_COUNT)
+	: pin_function_locks_(PIN_FUNCTION_REGISTER_COUNT)
 {
 	// Map gpio peripherals into memory
 	int fd = open("/dev/mem", O_RDWR | O_SYNC);
@@ -35,18 +36,43 @@ PinManager::PinManager()
       PROT_READ | PROT_WRITE,
       MAP_SHARED,
       fd,
-      GPIO_MEMORY_OFFSET);
+      GPIO_PHYSICAL_MEMORY_OFFSET);
 
   if (memory == MAP_FAILED) {
     throw std::runtime_error(SysUtils::GetErrorMessage());
   }
 
-  gpio_base_ = static_cast<volatile uint64_t*>(memory);
+  gpio_base_ = static_cast<volatile uint32_t*>(memory);
 }
 
 // TODO: unmap gpio memory
 PinManager::~PinManager() {}
 
-PinManager& PinManager::SetFunction(uint8_t pin_index, PinType pin_type) {}
+PinManager& PinManager::BindFunction(uint8_t pin_index, PinType pin_type) {
+  // Start critical section for editing gpio function register
+  size_t function_register_index = MakeFunctionRegisterIndex(pin_index);
+  std::lock_guard<std::mutex> lock(pin_function_locks_[function_register_index]);
+
+  // First, clear existing function. Then, set new function.
+  size_t shift_value = MakeShiftValue(pin_index);
+  gpio_base_[function_register_index] &= MakeClearFunctionValue(shift_value);
+  gpio_base_[function_register_index] |= MakeSetFunctionValue(shift_value, pin_type);
+}
+
+uint32_t PinManager::MakeClearFunctionValue(size_t shift_value) const {
+  return ~(0b111 << shift_value); 
+}
+
+uint32_t PinManager::MakeSetFunctionValue(size_t shift_value, PinType pin_type) const {
+  return static_cast<size_t>(pin_type) << shift_value;
+}
+
+size_t PinManager::MakeShiftValue(uint8_t pin_index) const {
+  return (pin_index % PINS_PER_FUNCTION_REGISTER) * BITS_PER_FUNCTION;
+}
+
+size_t PinManager::MakeFunctionRegisterIndex(uint8_t pin_index) const {
+  return pin_index / PINS_PER_FUNCTION_REGISTER;
+}
 
 } // namespace gpio
