4b523f57547a78acc60b904aacf880297d6b66c6
Trevor Assaf <bozkurtus@google.com>
2017-11-12 12:01:44 -0800

    Add I2c::DataLengthRegisterBits

---
+++ include/I2c/DataLengthRegisterBits.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdlib>
+
+namespace I2c
+{
+
+class DataLengthRegisterBits
+{
+public:
+	size_t GetDataLengthOffset() const;
+};
+} // namespace I2c
---
+++ src/I2c/DataLengthRegisterBits.cpp
@@ -0,0 +1,6 @@
+#include "I2c/DataLengthRegisterBits.h"
+
+namespace I2c
+{
+size_t DataLengthRegisterBits::GetDataLengthOffset() const { return 0; }
+} // namespace I2c
