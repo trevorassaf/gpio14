3f4dd07f7f8a16f40678c2c0769cab6f775f0fbf
Trevor Assaf <bozkurtus@google.com>
2017-11-12 12:58:35 -0800

    Add I2c::ClockDivideRegisterBits

---
+++ include/I2c/ClockDivideRegisterBits.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdlib>
+
+namespace I2c
+{
+class ClockDivideRegisterBits
+{
+public:
+	size_t GetClockDivideOffset() const;
+};
+} // namespace I2c
---
+++ src/I2c/ClockDivideRegisterBits.cpp
@@ -0,0 +1,6 @@
+#include "I2c/ClockDivideRegisterBits.h"
+
+namespace I2c
+{
+size_t ClockDivideRegisterBits::GetClockDivideOffset() const { return 0; }
+} // namespace I2c
