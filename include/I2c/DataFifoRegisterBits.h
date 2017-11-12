8c070ef2977856288824c0d77e308468c9432864
Trevor Assaf <bozkurtus@google.com>
2017-11-12 12:41:55 -0800

    Add I2c::DataFifoRegisterBits

---
+++ include/I2c/DataFifoRegisterBits.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdlib>
+
+namespace I2c
+{
+class DataFifoRegisterBits
+{
+public:
+	size_t GetDataFifoOffset() const;
+};
+} // namespace I2c
---
+++ src/I2c/DataFifoRegisterBits.cpp
@@ -0,0 +1,6 @@
+#include "I2c/DataFifoRegisterBits.h"
+
+namespace I2c
+{
+size_t DataFifoRegisterBits::GetDataFifoOffset() const { return 0; }
+} // namespace I2c
