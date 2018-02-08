30ced4c516b8865603b42a728b426f3d468adf20
Trevor Assaf <bozkurtus@google.com>
2018-02-07 21:13:20 -0800

    Fix MockFdOps

--- include/Utils/MockFdOps.h
+++ include/Utils/MockFdOps.h
@@ -12,6 +12,7 @@ namespace Utils
 typedef std::function<void(const char *path, Fd *outFd)> open_handler_t;
 typedef std::function<void(const Fd &fd, const uint8_t *buffer, size_t length)> write_handler_t;
 typedef std::function<void(const Fd &fd, uint8_t *buffer, size_t length)> read_handler_t;
+typedef std::function<void()> close_handler_t;
 
 class MockFdOps : public FdOps
 {
@@ -19,10 +20,12 @@ public:
 	MockFdOps(
 			open_handler_t openHandler,
 			write_handler_t writeHandler,
-			read_handler_t readHandler)
+			read_handler_t readHandler,
+			close_handler_t closeHandler)
 	: m_openHandler{std::move(openHandler)},
 		m_writeHandler{std::move(writeHandler)},
-		m_readHandler{std::move(readHandler)} {}
+		m_readHandler{std::move(readHandler)},
+	  m_closeHandler{std::move(closeHandler)}	{}
 
 	FdOpResult Open(const char *path, Fd *outFd)
 	{
@@ -42,10 +45,17 @@ public:
 			return FdOpResult::Ok();
 	}
 
+	FdOpResult Close()
+	{
+			m_closeHandler();
+			return FdOpResult::Ok();
+	}
+
 private:
 	open_handler_t m_openHandler;
 	write_handler_t m_writeHandler;
 	read_handler_t m_readHandler;
+	close_handler_t m_closeHandler;
 };
 
 } // namespace Utils
